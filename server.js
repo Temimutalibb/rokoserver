abcc73e5c84e621f978e74829fd555d5e556edab
Mutalibb <temitopking@icloud.com>
2024-10-20 07:28:29 -0700

    frontend link added

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://roko-note.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
